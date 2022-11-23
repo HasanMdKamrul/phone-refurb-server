5823f3e38e6035a045e98e164c6446817681c92e
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-11-23 22:15:37 +0100

    categories data saved and fetched in server side

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -15,6 +15,55 @@ app.use(express.json());
 // ** Test Api
 app.get("/", (req, res) => res.send("Server is running - phone-refurb"));
 
-// ** app listen
+// ********* DB CONNECTION *********
 
+const uri = `${process.env.DB_URI}`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// ** DB RUN
+const run = async () => {
+  try {
+    await client.connect();
+    console.log("DB Connected");
+  } finally {
+  }
+};
+run().catch((error) => console.log(error.message));
+
+// ** DB Collections
+
+const categoryCollection = client
+  .db("phone-refurb-db")
+  .collection("categories");
+
+// ** DB Collections
+
+// ** DB RUN
+
+// ** APIS ********
+
+app.get("/categories", async (req, res) => {
+  try {
+    const query = {};
+    const categories = await categoryCollection.find(query).toArray();
+    return res.send({
+      success: true,
+      data: categories,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+// ** APIS ********
+
+// ** app listen
 app.listen(port, () => console.log(`Server is running at ${port}`));
